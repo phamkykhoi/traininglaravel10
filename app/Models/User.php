cad52927db4b0c3b7b158c2e68c8fedf0c06ae82
phamkykhoi <phamkykhoi.info@gmail.com>
2023-11-21 21:15:01 +0700

    Update Code

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable
         'bio',
         'gender',
         'socials',
+        'avatar',
         'remember_token',
         'deleted_at',
         'created_at',
