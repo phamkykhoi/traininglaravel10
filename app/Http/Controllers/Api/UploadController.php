eb35df0c64f644a81552f3ebf6eaecd25d8dd60b
phamkykhoi <phamkykhoi.info@gmail.com>
2023-11-21 20:01:49 +0700

    Upload File

--- app/Http/Controllers/Api/UploadController.php
+++ app/Http/Controllers/Api/UploadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UploadRequest;
+use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
